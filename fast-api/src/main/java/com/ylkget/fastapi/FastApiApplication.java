44454f5c11a9313e1f9c8eb6b181d5592a4ff1fb
yang linkai <ylksty@163.com>
2021-03-11 08:21:22 +0800

    🎨 init

---
+++ fast-api/src/main/java/com/ylkget/fastapi/FastApiApplication.java
@@ -0,0 +1,13 @@
+package com.ylkget.fastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastApiApplication.class, args);
+	}
+
+}
---
+++ fast-api/src/test/java/com/ylkget/fastapi/FastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylkget.fastapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
